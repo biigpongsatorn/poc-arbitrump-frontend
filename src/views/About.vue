43212a90a1f621e44bf0743a4e6958b3008fd3db
ashvajit <rnJr9K2f2ax@JcrXbanyL-8pM-@gbU2>
2021-03-15 10:59:07 +0700

    First commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>
+      Add custom RPC to Metamask
+    </h2>
+    <div style="text-align: left; width: 500px; margin: 20px auto;">
+      <b>Network Name:</b> Arbitrum Testnet V3 <br>
+      <b>New RPC URL:</b> https://kovan3.arbitrum.io/rpc <br>
+      <b>ChainID:</b> 79377087078960 <br>
+      <b>Symbol:</b> ETH <br>
+      <b>Block Explorer URL:</b> https://explorer.arbitrum.io/#/ <br>
+    </div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,118 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="@/assets/logo.png" width="50px">
+    <h1>POC ERC20 On L2 (Arbitrum)</h1>
+    <br>
+    <h2>Address: {{ userAddr }}</h2>
+    <h2>Balance: {{ balance.div('1e18') }} TKN</h2>
+    <div v-if="userAddr">
+      <button @click="mint">
+        Mint
+      </button>
+      <br>
+      <a v-show="mintTxHash && !isMintSuccess" :href="`https://explorer.arbitrum.io/#/tx/${mintTxHash}`" target="_blank">
+        View transaction detail
+      </a>
+      <h3 v-show="isMintSuccess" style="color: green">
+        Mint Success
+      </h3>
+      <br>
+      <div style="width: 400px; margin: 0 auto;">
+        <hr>
+        <br>
+        <div>
+          <h3>Transfer</h3>
+          <input
+            v-model="inputDestAddr"
+            type="text"
+            placeholder="Destination Address"
+            style="margin-right: 5px;"
+          >
+          <input
+            v-model="inputAmount"
+            type="number"
+            placeholder="Amount"
+            style="margin-right: 5px;"
+          >
+          <button @click="transfer">
+            Transfer
+          </button>
+          <br><br>
+          <a v-show="transferTxHash && !isTransferSuccess" :href="`https://explorer.arbitrum.io/#/tx/${transferTxHash}`" target="_blank">
+            View transaction detail
+          </a>
+          <br>
+          <h3 v-show="isTransferSuccess" style="color: green">
+            Transfer Success
+          </h3>
+          <br>
+          <span>address for test transfer: 0xb1adceddb2941033a090dd166a462fe1c2029484</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Web3 from 'web3'
+import BigNumber from 'bignumber.js'
+import MockERC20 from '@/assets/contracts/MockERC20.json'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      web3: null,
+      erc20Contract: null,
+      userAddr: '',
+      balance: new BigNumber(0),
+      mintTxHash: '',
+      isMintSuccess: false,
+      inputDestAddr: '', // 0xb1adceddb2941033a090dd166a462fe1c2029484
+      inputAmount: 0,
+      transferTxHash: '',
+      isTransferSuccess: false
+    }
+  },
+  async mounted () {
+    this.web3 = new Web3(window.web3.currentProvider)
+    window.web3 = new Web3(window.web3.currentProvider)
+    window.ethereum.enable()
+    this.userAddr = (await this.web3.eth.getAccounts())[0]
+    this.erc20Contract = new this.web3.eth.Contract(MockERC20.abi, '0x601819ef8a1A4c6e90aa855f7e70DF8a7Dd3AFF7')
+    this.getUserBalance(this.userAddr)
+  },
+  methods: {
+    async getUserBalance (address = this.userAddr) {
+      if (address) {
+        const balance = await this.erc20Contract.methods.balanceOf(address).call()
+        this.balance = new BigNumber(balance)
+      }
+    },
+    mint () {
+      this.erc20Contract.methods.mint(this.userAddr, new BigNumber(100).times(1e18))
+        .send({ from: this.userAddr, gasLimit: 100000 })
+        .on('transactionHash', (hash) => {
+          this.isMintSuccess = false
+          this.mintTxHash = hash
+        })
+        .on('receipt', () => {
+          this.isMintSuccess = true
+          this.getUserBalance()
+        })
+    },
+    transfer () {
+      this.erc20Contract.methods.transfer(this.inputDestAddr, new BigNumber(this.inputAmount).times(1e18))
+        .send({ from: this.userAddr, gasLimit: 200000 })
+        .on('transactionHash', (hash) => {
+          this.isTransferSuccess = false
+          this.transferTxHash = hash
+        })
+        .on('receipt', () => {
+          this.isTransferSuccess = true
+          this.getUserBalance()
+        })
+    }
   }
 }
 </script>
